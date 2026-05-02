cd0cf6a39bddf2755b8621ccf6e80cc06a530dd3
ultimatevoltra <ultimatevoltra@gmail.com>
2026-05-02 12:29:55 +0600

    fix: default DATABASE_URL to SQLite if not set

--- packages/db/index.js
+++ packages/db/index.js
@@ -1,13 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
+
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = 'file:/tmp/dev.db';
+}
 
 const globalForPrisma = global;
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"]
+    log: process.env.NODE_ENV === 'development' ? ['query'] : []
   });
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
 }
